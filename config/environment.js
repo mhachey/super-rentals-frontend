efb9ddba6dccbe93fc8b741eaddd5e4d39918693
Mike Hachey <michael.g.hachey@gmail.com>
2022-04-24 18:12:43 -0400

    add Mapbox API access token

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function (environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibWljaGFlbC1nLWhhY2hleSIsImEiOiJjbDJkdWN0cmUxMXZkM2lwYXhzenFvMnIwIn0.7isKJ1HTiHtrz6hAUINizw';
+
   return ENV;
 };
