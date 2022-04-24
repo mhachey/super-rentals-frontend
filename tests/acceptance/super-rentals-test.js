563ccf9eb01f2eb4b1862950e1c6b12dbce17629
Mike Hachey <michael.g.hachey@gmail.com>
2022-04-24 17:38:43 -0400

    create Rental component w/ integration test

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -47,7 +47,7 @@ module('Acceptance | super rentals', function (hooks) {
     assert.strictEqual(currentURL(), '/about');
   });
 
-  test('navigating using the nav-bar', async function(assert) {
+  test('navigating using the nav-bar', async function (assert) {
     await visit('/');
 
     assert.dom('nav').exists();
